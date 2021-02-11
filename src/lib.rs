9f8686769df2df985d1101d8bb370f9d9e328e3b
Sergey Bargamon <sergey@bargamon.ru>
2021-02-12 00:45:43 +0200

    add Your Mom functionality. inspired by https://twitter.com/ftrain/status/1359138516681314311?s=21

--- src/lib.rs
+++ src/lib.rs
@@ -12,61 +12,104 @@ mod find_and_replace {
     fn naive() {
         assert_eq!(
             find_and_replace(
-                &"https://test.test/?fbclid=dsadsa&utm_source=fafa&utm_campaign=fafas&utm_medium=adsa".to_string()
+                &"https://test.test/?fbclid=dsadsa&utm_source=fafa&utm_campaign=fafas&utm_medium=adsa".to_string(),
+                &ProcessTypes::REMOVE
             ),  
             "https://test.test/"
         );
+        assert_eq!(
+            find_and_replace(
+                &"https://test.test/?fbclid=dsadsa&utm_source=fafa&utm_campaign=fafas&utm_medium=adsa".to_string(),
+                &ProcessTypes::YourMom
+            ),  
+            "https://test.test/?fbclid=your_mom&utm_source=your_mom&utm_campaign=your_mom&utm_medium=your_mom"
+        );
+
     }
     #[test]
     fn should_preserve_query() {
         assert_eq!(
             find_and_replace(
-                &"https://test.test/?abc=abc".to_string()
+                &"https://test.test/?abc=abc".to_string(),
+                &ProcessTypes::REMOVE
             ),  
             "https://test.test/?abc=abc"
         );
+        assert_eq!(
+            find_and_replace(
+                &"https://test.test/?abc=abc".to_string(),
+                &ProcessTypes::YourMom
+            ),  
+            "https://test.test/?abc=abc"
+        );
+
     }
     #[test]
     fn multiple_params() {
         assert_eq!(
             find_and_replace(
-                &"https://test.test/?abc=abc&fbclid=flksj".to_string()
+                &"https://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::REMOVE
             ),  
             "https://test.test/?abc=abc"
         );
+        assert_eq!(
+            find_and_replace(
+                &"https://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::YourMom
+            ),  
+            "https://test.test/?abc=abc&fbclid=your_mom"
+        );
+
     }
     #[test]
     fn multiple_links() {
         assert_eq!(
             find_and_replace(
-                &"https://test.test/?abc=abc&fbclid=flksj\nhttps://test.test/?abc=abc&fbclid=flksj".to_string()
+                &"https://test.test/?abc=abc&fbclid=flksj\nhttps://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::REMOVE
             ),  
             "https://test.test/?abc=abc\nhttps://test.test/?abc=abc"
         );
+        assert_eq!(
+            find_and_replace(
+                &"https://test.test/?abc=abc&fbclid=flksj\nhttps://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::YourMom
+            ),  
+            "https://test.test/?abc=abc&fbclid=your_mom\nhttps://test.test/?abc=abc&fbclid=your_mom"
+        );
+
     }
     #[test]
     fn multiple_links_and_text() {
         assert_eq!(
             find_and_replace(
-                &"some text here https://test.test/?abc=abc&fbclid=flksj here \nand herehttps://test.test/?abc=abc&fbclid=flksj".to_string()
+                &"some text here https://test.test/?abc=abc&fbclid=flksj here \nand herehttps://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::REMOVE
             ),  
             "some text here https://test.test/?abc=abc here \nand herehttps://test.test/?abc=abc"
         );
+        assert_eq!(
+            find_and_replace(
+                &"some text here https://test.test/?abc=abc&fbclid=flksj here \nand herehttps://test.test/?abc=abc&fbclid=flksj".to_string(),
+                &ProcessTypes::YourMom
+            ),  
+            "some text here https://test.test/?abc=abc&fbclid=your_mom here \nand herehttps://test.test/?abc=abc&fbclid=your_mom"
+        );
+
     }
 
 
 }
 
-pub fn find_and_replace(str: &String)-> String {
+pub fn find_and_replace(str: &String, process_type: &ProcessTypes)-> String {
     let mut finder = LinkFinder::new();
     finder.kinds(&[LinkKind::Url]);
     let mut res = str.clone();
     for link in finder.links(str){
         let l = Url::parse(link.as_str()).unwrap();
 
-        let query: Vec<(_, _)> = l.query_pairs()
-        .filter(|p| p.0 != "fbclid" && p.0 != "utm_source" && p.0 != "utm_campaign" && p.0 != "utm_medium")
-        .collect();
+        let query: Vec<(_, _)> = process_query(l.query_pairs(), process_type);
         
         let mut l2 = l.clone();
         l2.set_query(None);
@@ -82,4 +125,40 @@ pub fn find_and_replace(str: &String)-> String {
     return res;
 }
 
+pub enum ProcessTypes{
+    REMOVE,
+    YourMom
+}
+
+
+fn process_query(query: url::form_urlencoded::Parse<'_>, process_type: &ProcessTypes)-> Vec<(String, String)>{
+    match process_type {
+        ProcessTypes::REMOVE => {
+            return query
+            .filter(|p| !is_hit(&p.0))
+            .map(|p| (p.0.to_string(), p.1.to_string()))
+            .collect()
+        }
+        ProcessTypes::YourMom => {
+            return query.map(|p| if is_hit(&p.0) {(p.0.to_string(), "your_mom".to_string())} else {(p.0.to_string(), p.1.to_string())}).collect();
+        }
+    } 
+}
+
+
+#[cfg(test)]
+mod is_hit {
+    use super::*;
+
+    #[test]
+    fn test_all() {
+        assert!(is_hit("fbclid"));
+        assert!(is_hit("utm_source"));
+        assert!(is_hit("utm_campaign"));
+        assert!(is_hit("utm_medium"));
+    }
+}
+fn is_hit(p: &str) -> bool {
+    p == "fbclid" || p == "utm_source" || p == "utm_campaign" || p == "utm_medium"
+}
 
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 use std::thread;
 use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
-use lib::find_and_replace;
+use lib::{find_and_replace, ProcessTypes};
 
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
@@ -15,7 +15,7 @@ fn main() {
         match ctx.get_contents() {
             Ok(v) => {
                 if buff != v {
-                    ctx.set_contents(find_and_replace(&v)).unwrap();
+                    ctx.set_contents(find_and_replace(&v, &ProcessTypes::REMOVE)).unwrap();
                     buff = v;
                 }
             },
