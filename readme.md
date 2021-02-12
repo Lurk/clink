f3c7c8d58e07c8d360c78de0b78439d53062a41f
Sergey Bargamon <sergey@bargamon.ru>
2021-02-12 12:27:21 +0200

    use CLINK_MODE env to set update mode

--- readme.md
+++ readme.md
@@ -1,9 +1,19 @@
 # clink
 
-Automatically removes fbclid, utm_source, utm_campaign, utm_medium GET params from links in clipboard 
+Automatically updates fbclid, utm_source, utm_campaign, utm_medium GET params from links in clipboard 
 
-## Your mom mode
+## Modes
+You can choose mode for clink by the setting env variable CLINK_MODE
 
+### Remove mode (default)
+
+CLINK_MODE=Remove
+removes fbclid, utm_source, utm_campaign, utm_medium GET params from links in clipboard
+
+### Your mom mode
+
+CLINK_MODE=YourMom
+sets fbclid, utm_source, utm_campaign and utm_medium GET params to "your_mom" in links that are in clipboard
 inspired by this [tweet](https://twitter.com/ftrain/status/1359138516681314311?s=21)
 
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate clipboard;
 mod lib;
 
 use std::time::Duration;
+use std::env;
 use std::thread;
 use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
@@ -11,11 +12,18 @@ use lib::{find_and_replace, Mode};
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let mut buff = "".to_string();
+    let mode:Mode;
+    match env::var("CLINK_MODE").unwrap_or("Remove".to_string()).as_str() {
+        "Remove" => mode = Mode::Remove,
+        "YourMom" => mode = Mode::YourMom,
+        _ => mode = Mode::Remove
+    }
+
     loop {
         match ctx.get_contents() {
             Ok(v) => {
                 if buff != v {
-                    ctx.set_contents(find_and_replace(&v, &Mode::Remove)).unwrap();
+                    ctx.set_contents(find_and_replace(&v, &mode)).unwrap();
                     buff = v;
                 }
             },
