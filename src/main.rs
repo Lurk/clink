8c8774a5ff78793c8d62bed9e9c3bcc78bd2a118
Sergey Bargamon <sergey@bargamon.ru>
2021-02-14 12:21:34 +0200

    Switch from env to command line params.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
-extern crate clipboard;
+mod mode;
+mod utils;
 
-mod lib;
-
+use self::mode::Mode;
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
-use lib::{find_and_replace, Mode};
-use std::env;
+use rustop::opts;
 use std::thread;
 use std::time::Duration;
+use utils::find_and_replace;
 
 fn main() {
+    let (args, _rest) = opts! {
+        command_name "clink";
+        synopsis "Clink automatically cleans url in your clipboard";    
+        version env!("CARGO_PKG_VERSION");     
+        opt verbose:bool, desc:"Be verbose.";           
+        opt mode: Mode = Mode::Remove, desc:"Mode of clink. Available \"remove\" and \"your_mom\" modes"; 
+    }.parse_or_exit();
+
+    if args.verbose {
+        println!("Clink is running with {} mode.", args.mode);
+    }
+
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let mut buff = "".to_string();
-    let mode: Mode = match env::var("CLINK_MODE")
-        .unwrap_or_else(|_| "Remove".to_string())
-        .as_str()
-    {
-        "Remove" => Mode::Remove,
-        "YourMom" => Mode::YourMom,
-        _ => Mode::Remove,
-    };
 
     loop {
         match ctx.get_contents() {
             Ok(v) => {
                 if buff != v {
-                    ctx.set_contents(find_and_replace(&v, &mode)).unwrap();
+                    ctx.set_contents(find_and_replace(&v, &args.mode)).unwrap();
                     buff = v;
                 }
             }
